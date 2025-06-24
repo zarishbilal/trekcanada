04660aa968bb17b021062aad580f749b92a33674
zarishbilal <zarishsuleman@gmail.com>
2025-06-24 11:50:37 -0600

    updated server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+// server.js (NO express needed)
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
